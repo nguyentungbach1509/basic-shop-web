31b5fc6f81885f0dc455e3b34fc80cb9a5a531aa
nguyentungbach1509 <78577517+nguyentungbach1509@users.noreply.github.com>
2023-07-02 10:32:09 -0400

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import firebase from 'firebase';
 
 const firebaseApp = firebase.initializeApp ({
     
-    apiKey: "AIzaSyCZcGm5LG4XlD-7ikitWIDRSmJtfbZLLsI",
-    authDomain: "basic-shop-web.firebaseapp.com",
-    projectId: "basic-shop-web",
-    storageBucket: "basic-shop-web.appspot.com",
-    messagingSenderId: "616427752039",
-    appId: "1:616427752039:web:ef5039f4f62b4a29c8a2c2",
-    measurementId: "G-MZPT56QG9X"
+    apiKey: "AIzaSyBW9Z2rKBJJYa-pTvp8jjYudZ6FWdcWYQY",
+    authDomain: "basic-shop-app-4b835.firebaseapp.com",
+    projectId: "basic-shop-app-4b835",
+    storageBucket: "basic-shop-app-4b835.appspot.com",
+    messagingSenderId: "565265265294",
+    appId: "1:565265265294:web:6b119fe510b038f2270fdf",
+    measurementId: "G-K04T79JC67"
 
 })
 
